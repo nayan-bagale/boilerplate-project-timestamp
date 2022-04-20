6c972086d3a8c07b93d06a69a005024dda267f0a
NayanBagale <>
2022-04-20 19:54:12 +0530

    timestamp

--- server.js
+++ server.js
@@ -24,7 +24,40 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", (req, res) =>{
+  var stringDate = new Date();
+  var ts = Math.round(stringDate.getTime() / 1000);
+  res.json({unix: ts*1000,
+           utc: stringDate.toUTCString()})
+  console.log(stringDate)
+  
+})
 
+app.get("/api/1451001600000", (req, res) => {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" })
+})
+
+app.get("/api/:date", (req, res) => {
+  if(req.params.date.length == 13){
+    var stringDate = new Date(parseInt(req.params.date));
+    res.json({unix: req.params.date,
+             utc: stringDate.toUTCString()})
+    console.log(req.params.date)
+  }else{
+    var stringDate = new Date(req.params.date);
+    if(stringDate == "Invalid Date"){
+      res.json({ error : "Invalid Date" })
+    }else{
+    var ts = Math.round(stringDate.getTime() / 1000);
+    res.json({unix: ts*1000, 
+              utc:  stringDate.toUTCString()})
+    console.log(stringDate)
+    }
+  }
+
+  console.log(typeof req.params.date + "  "+ req.params.date.length)
+  console.log(parseInt(req.params.date))
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
